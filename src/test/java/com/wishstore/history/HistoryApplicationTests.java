d2d4657105457fde2d7bb1d280e3ea58b757ea4b
Juanes_URBA <024juanurrego@gmail.com>
2026-09-15 01:57:26 -0500

    chore: initialize history service project

--- src/test/java/com/wishstore/history/HistoryApplicationTests.java
+++ src/test/java/com/wishstore/history/HistoryApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WishstoreHistoryServiceApplicationTests {
+class HistoryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
